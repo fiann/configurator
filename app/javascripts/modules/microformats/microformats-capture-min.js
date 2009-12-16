15d999b1489af9498ceb4c43a820875cff0ef91e
fiann <fiann@ohagan.me>
2009-12-16 09:21:24 +0000

    Update microformats module to core

--- app/javascripts/modules/microformats/microformats-capture-min.js
+++
@@ -1,1 +0,0 @@
-"use strict";YUI.add("microformats-capture",function(a){},"2.0.0",{requires:["microformats","hauthentication-capture","hpage-capture","hproduct-capture","hpurchase-capture"],after:["microformats"]});
--- app/models/plugin/microformat.rb
+++ app/models/plugin/microformat.rb
@@ -2,6 +2,7 @@ class Plugin::Microformat < Plugin
 
   def modules
     [
+      TagModule.new('microformats/microformats-api', :library),
       TagModule.new('microformats/hauthentication-capture', :data_capture),
       TagModule.new('microformats/hpage-capture', :data_capture),
       TagModule.new('microformats/hproduct-capture', :data_capture),
