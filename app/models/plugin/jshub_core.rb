aeccebd9ab74ca250283bbf4b9982a3dabcd10bf
fiann <fiann@ohagan.me>
2010-03-12 16:17:30 +0000

    Add URL parsing utils to core build

--- app/models/plugin/jshub_core.rb
+++ app/models/plugin/jshub_core.rb
@@ -5,6 +5,7 @@ class Plugin::JshubCore < Plugin
       TagModule.new('debug/debug', :library),
       TagModule.new('hub/hub', :core),
       TagModule.new('logger/logger', :core_support),
+      TagModule.new('hub/utils', :core_support),
       TagModule.new('hub/technographics', :data_capture),
       TagModule.new('hub/load-triggers', :triggers)
     ]
