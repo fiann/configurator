ea86ed3e787ace98a0674597d3cb9afc569d3eae
fiann <fiann@ohagan.me>
2010-03-15 09:14:53 +0000

    Updated with search engine referrer

--- app/javascripts/hub/technographics-debug.js
+++ app/javascripts/hub/technographics-debug.js
@@ -24,6 +24,26 @@
     type: 'data-capture'
   };
   
+  var searchEngineList = {
+    "about.com" : "terms", // About
+    "alice.com" : "qs", // Alice
+    "alltheweb.com" : "q", // Alltheweb
+    "altavista.com" : "q", // Altavista
+    "aol\\.[^\\/]+" : ["encquery",  "q", "query"], // AOL
+    "ask.com" : "q", // Ask
+    "bing.com" : "q", // Bing
+    "cnn.com" : "query", // CNN
+    "google\\.[^\\/]+" :	"q", // Google
+    "lycos.com" : "query", // Lycos
+    "mamma.com" : "query", // Mamma
+    "msn.com" : "q", // MSN
+    "netscape.com" : "query", // Netscape
+    "online.onetcenter.org" :	"qt", // O*NET
+    "search.com" : "q", // Search
+    "terra.com" : "query", // Terra
+    "yahoo.com" : "p" // Yahoo
+  };
+  
   /*
    * First trigger an event to show that the plugin is being registered
    */
@@ -68,12 +88,37 @@
     
     // Document referrer is the default for hPage.referrer
     found.referrer = document.referrer;
-    if (!data.referrer) {
+    if (!data['page-referrer']) {
       data['page-referrer'] = found.referrer;
     }
     
+    // Look for search engine in referrer URL
+    if (data['page-referrer']) {
+      var referrer = data['page-referrer'];
+      var testReferrer = function (param, host) {
+        var hnRegexp = new RegExp("^http(s)?:\\/\\/(www\\.)?((.+\\.)?" + host + ")\\/"),
+          qsRegexp = new RegExp("\\?(.+[&;])?" + param + "=([^&;]+)([&;].*)?$");
+        if (hnRegexp.test(referrer) && qsRegexp.test(referrer)) {
+          jsHub.trigger("nat-search-ref", {
+            "search-keyword" : decodeURIComponent(referrer.match(qsRegexp)[2].replace("+", " ")),
+            "external-referrer" : referrer,
+            "search-engine" : referrer.match(hnRegexp)[3]
+          });
+        }
+      };
+      jsHub.util.each(searchEngineList, function (param, host) {
+        if (typeof param === "string") {
+          testReferrer(param, host);
+        } else {
+          jsHub.util.each(param, function (p) {
+            testReferrer(p, host);
+          });
+        }
+      });
+    }
+    
     // and send to output plugins
-    jsHub.trigger("technographic-parse-complete", data);
+    jsHub.trigger("technographic-parse-complete", found);
 
     return data;
   };
