dfbc91e3e04376b14410a211ad35d2d39c0803ca
fiann <fiann@ohagan.me>
2010-05-20 13:46:11 +0100

    Make sure production settings are used on jshub.org

--- config/environments/jshub.rb
+++ config/environments/jshub.rb
@@ -1,19 +1,26 @@
 # Settings specified here will take precedence over those in config/environment.rb
 
-# In the development environment your application's code is reloaded on
-# every request.  This slows down response time but is perfect for development
-# since you don't have to restart the webserver when you make code changes.
-config.cache_classes = false
+# The production environment is meant for finished, "live" apps.
+# Code is not reloaded between requests
+config.cache_classes = true
 
-# Log error messages when you accidentally call methods on nil.
-config.whiny_nils = true
+# Enable threaded mode
+# config.threadsafe!
 
-# Show full error reports and disable caching
-config.action_controller.consider_all_requests_local = true
-config.action_view.debug_rjs                         = true
-config.action_controller.perform_caching             = false
+# Use a different logger for distributed setups
+# config.logger = SyslogLogger.new
 
-# Don't care if the mailer can't send
+# Full error reports are disabled and caching is turned on
+config.action_controller.consider_all_requests_local = false
+config.action_controller.perform_caching             = true
+
+# Use a different cache store in production
+# config.cache_store = :mem_cache_store
+
+# Enable serving of images, stylesheets, and javascripts from an asset server
+# config.action_controller.asset_host                  = "http://assets.example.com"
+
+# Disable delivery errors, bad email addresses will be ignored
 config.action_mailer.raise_delivery_errors = false
 
 # #315 - New in 2.2 use a prefix (matches Passenger RailsBaseURI)
